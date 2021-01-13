bf9a78e4e038674a2185276354ca17faa995e8e9
Andrew Jernigan <andrewjernigan@MacBook-Pro.fios-router.home>
2021-01-13 15:08:07 -0500

    cleaned up

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import HeaderDesk from "./components/header/HeaderDesk";
 
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 function App() {
   const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('sm'));
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
   return (
     <div className="App">
-      { matches ? <HeaderDesk /> : <HeaderMobile />}
+      {matches ? <HeaderDesk /> : <HeaderMobile />}
       <Main />
       <Footer />
     </div>
